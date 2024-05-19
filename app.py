20e759bd5d1fd024b1253b67aaee3b7043c12cc1
YaserFazil <frfvipf@gmail.com>
2024-05-19 03:13:15 +0300

    Made some updates in app.py file

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from botocore.exceptions import NoCredentialsError
 import os
 import math
 import zipfile
+import smtplib
+from email.mime.text import MIMEText
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,7 +22,7 @@ BUCKET_NAME = os.getenv("BUCKET_NAME")
 
 
 # Average compression speed in MB/s (adjust as needed)
-COMPRESSION_SPEED_MB_PER_SEC = 500
+COMPRESSION_SPEED_MB_PER_SEC = 1500
 
 
 PUBLIC_BUCKET_URL = os.getenv("PUBLIC_BUCKET_URL")
@@ -64,58 +66,87 @@ def get_folder_stats():
         return jsonify({"error": "Username not provided"}), 400
 
     try:
-        response = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=f"{username}/images/")
-        if "Contents" in response:
-            object_count = len(response["Contents"])
-            total_size = sum(obj["Size"] for obj in response["Contents"])
-            total_size_mb = total_size / (1024 * 1024)
-            estimated_time_to_zip = (
-                total_size_mb / COMPRESSION_SPEED_MB_PER_SEC
-                if COMPRESSION_SPEED_MB_PER_SEC > 0
-                else 0
-            )
-            return (
-                jsonify(
-                    {
-                        "username": username,
-                        "object_count": object_count,
-                        "total_size_bytes": total_size,
-                        "total_size_kb": total_size / 1024,
-                        "total_size_mb": total_size / (1024 * 1024),
-                        "total_size_gb": total_size / (1024 * 1024 * 1024),
-                        "estimated_time_to_zip_minutes": math.ceil(
-                            estimated_time_to_zip * 60
-                        ),
-                    }
-                ),
-                200,
-            )
-        else:
-            return (
-                jsonify(
-                    {
-                        "username": username,
-                        "object_count": 0,
-                        "total_size_bytes": 0,
-                        "total_size_kb": 0,
-                        "total_size_mb": 0,
-                        "total_size_gb": 0,
-                        "estimated_time_to_zip_minutes": 0,
-                    }
-                ),
-                200,
-            )
+        paginator = s3.get_paginator("list_objects_v2")
+        page_iterator = paginator.paginate(
+            Bucket=BUCKET_NAME, Prefix=f"{username}/images/"
+        )
+
+        image_names = []
+        total_size = 0
+        object_count = 0
+
+        for page in page_iterator:
+            if "Contents" in page:
+                objects = page["Contents"]
+                # Extract the image names
+                image_names.extend(
+                    item["Key"].split("images/")[1]
+                    for item in objects
+                    if "images/" in item["Key"]
+                )
+                object_count += len(objects)
+                total_size += sum(obj["Size"] for obj in objects)
+
+        total_size_mb = total_size / (1024 * 1024)
+        estimated_time_to_zip = (
+            total_size_mb / COMPRESSION_SPEED_MB_PER_SEC
+            if COMPRESSION_SPEED_MB_PER_SEC > 0
+            else 0
+        )
+
+        return (
+            jsonify(
+                {
+                    "username": username,
+                    "object_count": object_count,
+                    "total_size_bytes": total_size,
+                    "total_size_kb": total_size / 1024,
+                    "total_size_mb": total_size_mb,
+                    "total_size_gb": total_size / (1024 * 1024 * 1024),
+                    "estimated_time_to_zip_minutes": math.ceil(
+                        estimated_time_to_zip * 60
+                    ),
+                    "images_list": image_names,
+                }
+            ),
+            200,
+        )
     except NoCredentialsError:
         return jsonify({"error": "Credentials not available"}), 500
     except Exception as e:
+        print(f"Error when getting stats: {e}")
         return jsonify({"error": str(e)}), 500
+
+
+# Endpoint to check if images.zip exists in {username} folder inside AWS S3 bucket
+@app.route("/check_zip", methods=["GET"])
+def check_zip_file():
+    username = request.args.get("username")
+    if not username:
+        return jsonify({"error": "Username not provided"}), 400
+
+    zip_file_key = f"{username}/images.zip"
+    try:
+        response = s3.head_object(Bucket=BUCKET_NAME, Key=zip_file_key)
+        # If head_object does not raise an exception, the file exists
+        zip_file_url = f"https://{BUCKET_NAME}.s3.amazonaws.com/{zip_file_key}"
+        return jsonify({"is_available": True, "download_link": zip_file_url}), 200
+    except s3.exceptions.ClientError as e:
+        # If a 404 error is raised, the file does not exist
+        if e.response["Error"]["Code"] == "404":
+            return jsonify({"is_available": False}), 200
+        else:
+            # Handle other errors
+            print(f"Error when checking for zip file: {e}")
+            return jsonify({"error": str(e)}), 500
 
 
 @app.route("/zip", methods=["GET"])
 def zip_images_folder():
     username = request.args.get("username")
-    if not username:
-        return jsonify({"error": "Username not provided"}), 400
+    email = request.args.get("email")
+    if not username and not email:
+        return jsonify({"error": "Username or Email not provided"}), 400
 
     try:
         # Create in-memory buffer for zip file
@@ -154,12 +185,41 @@ def zip_images_folder():
 
         # Generate public link for downloading the zip file
         download_link = PUBLIC_BUCKET_URL + s3_zip_key
-
+        send_email(email)
         return jsonify({"download_link": download_link}), 200
     except NoCredentialsError:
+        send_email(
+            email,
+            body=f"Error occured while zipping your images. Please contact the Admin. Error: Credentials not available",
+        )
         return jsonify({"error": "Credentials not available"}), 500
     except Exception as e:
+        send_email(
+            email,
+            body=f"Error occured while zipping your images. Please try again later or contact the Admin. Error: {e}",
+        )
         return jsonify({"error": str(e)}), 500
+
+
+def send_email(
+    recipient,
+    subject="Soniclister Images Zipping process update",
+    body="Congrats! Your images zipped successfully!",
+):
+    sender = os.getenv("SENDER_GMAIL")
+    password = os.getenv("SENDER_GMAIL_APP_PASSWORD")
+    try:
+        msg = MIMEText(body)
+        msg["Subject"] = subject
+        msg["From"] = sender
+        msg["To"] = recipient
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
+            smtp_server.login(sender, password)
+            smtp_server.sendmail(sender, recipient, msg.as_string())
+            smtp_server.quit()
+        print("Message sent!")
+    except Exception as e:
+        print(e)
 
 
 @app.route("/")
@@ -168,4 +228,4 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
