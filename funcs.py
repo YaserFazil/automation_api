0256966e57d81d9c708edecac608642d5b7425d1
YaserFazil <frfvipf@gmail.com>
2024-05-24 13:14:12 +0300

    added dpi to fetch_barcode function in funcs.py file

--- funcs.py
+++ funcs.py
@@ -133,6 +133,7 @@ def fetch_barcode(data):
         "unit": "Min",
         "imagetype": "Png",
         "rotation": 90,
+        "dpi": 150,
     }
     response = requests.get(base_url, params=params)
 
