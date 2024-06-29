d0cf2ccb86252fc38642d89ee8994ce5f7a4cae2
YaserFazil <frfvipf@gmail.com>
2024-06-29 05:16:13 +0300

    updated

--- app.py
+++ app.py
@@ -650,15 +650,12 @@ def insert_products_mementodb(memento_lib_id, memento_token, memento_entryid, da
         scrape_status = "Scrape Failed"
         images = []
         msrps = []
-        print("Here 1")
         if len(data["shopping_results"]) > 0:
-            print("Here 2")
             # Determine scrape status based on the shopping_results
             scrape_status = "Manual Entry Data Scraped"
             for result in data["shopping_results"]:
                 images.append(result["thumbnail"])
                 msrps.append(result["extracted_price"])
-            print("Here 3: ", images, msrps)
             images_list = create_entries_products_for_images(images)
             msrps_list = create_entries_products_for_msrps(msrps)
             fields = (
@@ -673,7 +670,6 @@ def insert_products_mementodb(memento_lib_id, memento_token, memento_entryid, da
                     }
                 ]
             )
-            print("Here is fields: ", fields)
         payload = json.dumps({"fields": fields})
         headers = {"Content-Type": "application/json"}
         response = requests.request("PATCH", url, headers=headers, data=payload)
--- products_payload.py
+++ products_payload.py
@@ -1,7 +1,7 @@
 def create_entries_products_for_images(image_links):
     entries_products = []
     id_sequence = [28, 30, 32, 34, 36, 38, 40, 42, 44, 46]  # The id sequence as given
-    for index, link in enumerate(image_links):
+    for index, link in enumerate(image_links[:10]):
         entry = {
             "id": id_sequence[index],
             "name": f"Online Image {index + 1}",
@@ -15,7 +15,7 @@ def create_entries_products_for_images(image_links):
 def create_entries_products_for_msrps(msrps):
     entries_products = []
     id_sequence = [60, 61, 62, 63, 64, 71, 72, 77, 78, 79]  # The id sequence as given
-    for index, msrp in enumerate(msrps):
+    for index, msrp in enumerate(msrps[:10]):
         entry = {
             "id": id_sequence[index],
             "name": f"MSRP {index + 1}",
@@ -24,21 +24,3 @@ def create_entries_products_for_msrps(msrps):
         }
         entries_products.append(entry)
     return entries_products
-
-
-images = [
-    "asldkjf",
-    "kfdsj",
-    "lskdjf",
-    "lskjdf",
-    "lskdjf",
-    "sldjf",
-    "slkfdj",
-    "lskfdj",
-    "sfkj",
-    "skdjflk",
-]
-
-images_links = create_entries_products_for_images(images)
-msrps = create_entries_products_for_msrps(images)
-print(images_links + msrps)
