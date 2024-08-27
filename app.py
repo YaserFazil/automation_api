d93adb7778dc12aea3b856542e9c74dc93131277
YaserFazil <frfvipf@gmail.com>
2024-08-27 07:34:47 +0300

    tiny updates

--- app.py
+++ app.py
@@ -667,7 +667,7 @@ def search_products():
             {"status": "failed", "msg": "title or image is required parameter!"}
         )
     if image is not None:
-        print("Image: ", image)
+        print("Image: ", image[0], type(image))
         query = glens_results(image[0])
         print("Title from glens: ", query)
         sleep(5)
