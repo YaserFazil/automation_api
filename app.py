de11eb5e367b2a46cbd5c6ff9e7136d801fdd246
YaserFazil <frfvipf@gmail.com>
2024-09-15 01:22:45 +0300

    nothing special

--- app.py
+++ app.py
@@ -282,7 +282,7 @@ def hello_world():
 def fnsku_to_asin_logic_us_amz(product_code, entry_id, memento_lib_id, memento_token):
     with lock:
         # Define the endpoint
-        url = f"{os.getenv("NGROK_US_LAPTOP_ENDPOINT")}/product-scraper"
+        url = f"{os.getenv('NGROK_US_LAPTOP_ENDPOINT')}/product-scraper"
 
         # Define the parameters for the GET request
         params = {
