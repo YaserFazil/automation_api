b04df2a55dc8ae8a283f09e2a60080855ed0096c
YaserFazil <frfvipf@gmail.com>
2024-10-14 17:35:47 +0430

    a lot of crazy changes

---
+++ another.py
@@ -0,0 +1,88 @@
+import requests
+import json
+import time
+import math 
+import hashlib
+
+SEARCH_BARCODE_HEADERS = {
+    'User-Agent': 'okhttp/4.9.1',
+}
+
+SEARCH_BARCODE_ENDPOINT = 'https://match-visualsearch-ca.amazon.com/vsearch/2.0'
+
+def gen_authtoken(timestamp):
+    secret = 'efb8826e4544bbab8992d757ec8b1116'
+    username = 'amzn-mbl-cscan-ca'
+    application = 'amzn-mbl-cscan-ca'
+
+    sha512_hash = hashlib.sha512()
+    data = secret + username + application + str(timestamp)
+    sha512_hash.update(data.encode())
+
+    return sha512_hash.hexdigest().upper()
+
+def search_barcode(barcode):
+    #TODO: Implement https://match-visualsearch-ca.amazon.com/vsearch/2.0 Body request
+    timestamp = math.floor(time.time())
+    authtoken = gen_authtoken(timestamp)
+    print(authtoken)
+
+    body_params = {
+        'clientMessageVersion': '1.0',
+        'orientation': '90',
+        'uiMode': 'barcode_scanner',
+        'application': 'amzn-mbl-cscan-ca',
+        'lang': 'en_CA',
+        'ts': timestamp,
+        'username': 'amzn-mbl-cscan-ca',
+        'groupingId': '5045eb49-d8bb-4cc5-ad5e-6796e894b876',
+        'query_metadata' : '',
+        'vsearch_params_json': '',
+        'authtoken': authtoken
+    }
+
+    query_metdata = {
+        "amznSessionId":"135-7799391-5389824",
+        "clientVersion":"28.16.0.100",
+        "cardsVersion":"1.0",
+        "clientMessageVersion":"1.0",
+        "amznDirectedCustomerId":"",
+        "clientDeviceId":"06820589-f27d-40a0-ac10-b837d8b231ff", # changes
+        "clientDevice":"Android - TECNO KF6i", # changes 
+        "deviceManufacturer":"TECNO MOBILE", # changes 
+        "clientDeviceVersion":"11",
+        "intentScore":"6.000000",
+        "clientId":"616076d7-be9c-4274-a566-ca80ddf70946", # changes
+        "intentId":"0","initialPayloadSize":"0",
+        "version":"0.9",
+        "extra":"0",
+    }
+
+    vsearch_params_json = {
+        "occipital":{
+            "params":{
+                "barcode":barcode,
+                "barcodeType":"CODE_128"
+            }
+        }
+    }
+
+    body_params['query_metadata'] = json.dumps(query_metdata)
+    body_params['vsearch_params_json'] = json.dumps(vsearch_params_json)
+
+    res = requests.post(SEARCH_BARCODE_ENDPOINT, 
+                        headers=SEARCH_BARCODE_HEADERS, data=body_params)
+    
+    if res.ok:
+        converted_barcode = res.json()['occipital']['searchResult'][0]['properties']['convertedBarcodes'][0]
+        print(f'Converted barcode : {converted_barcode}')
+
+        return converted_barcode
+    else:
+        print(res.status_code)
+        print(res.text)
+    
+    return ''
+
+if __name__ == '__main__':
+    search_barcode("x00317fjlz".upper())
--- app.py
+++ app.py
@@ -42,47 +42,47 @@ canada_lock = Lock()  # Lock for Canadian automation
 usa_lock = Lock()     # Lock for USA automation
 
 
-# Decoding helper function
-def decode_content(content):
-    """Attempt to decode the content using various encodings."""
-    encodings = ['utf-8', 'ISO-8859-1', 'latin1']
-    for enc in encodings:
-        try:
-            return content.decode(enc), enc
-        except UnicodeDecodeError:
-            continue
-    return None, None  # If none of the encodings work, return None
+# # Decoding helper function
+# def decode_content(content):
+#     """Attempt to decode the content using various encodings."""
+#     encodings = ['utf-8', 'ISO-8859-1', 'latin1']
+#     for enc in encodings:
+#         try:
+#             return content.decode(enc), enc
+#         except UnicodeDecodeError:
+#             continue
+#     return None, None  # If none of the encodings work, return None
 
-# Interception and JSON saving function
-def response(flow: http.HTTPFlow) -> None:
-    # Check if the request is to the desired endpoint
-    if "match-visualsearch-ca.amazon.com" in flow.request.url:
-        # Intercepting and logging responses
-        print(f"\n[RESPONSE] {flow.request.url}")
-        print(f"Status Code: {flow.response.status_code}")
-        print(f"Headers: {flow.response.headers}")
+# # Interception and JSON saving function
+# def response(flow: http.HTTPFlow) -> None:
+#     # Check if the request is to the desired endpoint
+#     if "match-visualsearch-ca.amazon.com" in flow.request.url:
+#         # Intercepting and logging responses
+#         print(f"\n[RESPONSE] {flow.request.url}")
+#         print(f"Status Code: {flow.response.status_code}")
+#         print(f"Headers: {flow.response.headers}")
         
-        # Try to decode the response body
-        decoded_body, encoding = decode_content(flow.response.content)
-        if decoded_body:
-            print(f"Response Body (decoded with {encoding}): {decoded_body}")
+#         # Try to decode the response body
+#         decoded_body, encoding = decode_content(flow.response.content)
+#         if decoded_body:
+#             print(f"Response Body (decoded with {encoding}): {decoded_body}")
             
-            # Attempt to parse the decoded body as JSON
-            try:
-                json_data = json.loads(decoded_body)  # Ensure it's in a JSON format
-                # Define the path where you want to save the JSON file
-                json_file_path = os.path.join('./', 'response.json')
+#             # Attempt to parse the decoded body as JSON
+#             try:
+#                 json_data = json.loads(decoded_body)  # Ensure it's in a JSON format
+#                 # Define the path where you want to save the JSON file
+#                 json_file_path = os.path.join('./', 'response.json')
                 
-                # Empty the file before writing (open in 'w' mode truncates the file)
-                with open(json_file_path, 'w', encoding='utf-8') as json_file:
-                    # Save the JSON data to the file
-                    json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+#                 # Empty the file before writing (open in 'w' mode truncates the file)
+#                 with open(json_file_path, 'w', encoding='utf-8') as json_file:
+#                     # Save the JSON data to the file
+#                     json.dump(json_data, json_file, ensure_ascii=False, indent=4)
                 
-                print(f"JSON saved to {json_file_path}")
-            except json.JSONDecodeError:
-                print("Failed to decode response body as JSON")
-        else:
-            print(f"Response Body: Could not decode, binary content detected")
+#                 print(f"JSON saved to {json_file_path}")
+#             except json.JSONDecodeError:
+#                 print("Failed to decode response body as JSON")
+#         else:
+#             print(f"Response Body: Could not decode, binary content detected")
 
 import time
 from threading import Lock
@@ -884,5 +884,5 @@ def insert_products_mementodb(
 
 if __name__ == "__main__":
     # Start mitmdump in the background
-    start_mitmdump_in_background()
+    # start_mitmdump_in_background()
     app.run(debug=True)
