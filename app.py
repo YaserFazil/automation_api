ff0b99bc3474513e03945efaac3f2e988b76b71c
YaserFazil <frfvipf@gmail.com>
2024-10-19 19:04:22 +0430

    nothing

--- app.py
+++ app.py
@@ -744,7 +744,7 @@ def product_scraper():
     elif results == [] and usamazon_status_code != 200:
         print("Hello it's failed usamazon automation request")
         update_memento_entry(memento_lib_id, memento_token, memento_entryid)
-    elif results == []:
+    elif results == ([] or None or {}):
         update_memento_entry(memento_lib_id, memento_token, memento_entryid)
     return (
         jsonify({"message": "You have access to this endpoint", "items": results}),
